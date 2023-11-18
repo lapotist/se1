b84ea175f913bf78df4b0fa429a619dcec9631b5
lapotist <lapotist@gmail.com>
2023-11-18 22:50:39 +0800

    diamond knife textures fixed

--- src/main/java/lapotist/se1/ModItems.java
+++ src/main/java/lapotist/se1/ModItems.java
@@ -2,12 +2,10 @@ package lapotist.se1;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ShovelItem;
-import net.minecraft.item.SwordItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -28,18 +26,24 @@ public class ModItems {
             new Item(new FabricItemSettings()),
             "diamond_plate");
 
+    public static final Item REINFORCED_STICK = register(
+            new Item(new FabricItemSettings()),
+            "reinforced_stick");
+
     public static final Item DIAMOND_KNIFE = register(
             new SwordItem(DiamondTools.INSTANCE,15 , 15.0F, new FabricItemSettings()),
             "diamond_knife");
 
-    public static void AddToTools(Item item) {
+    public static void AddToGroup(Item item, RegistryKey<ItemGroup> group) {
         ItemGroupEvents
-                .modifyEntriesEvent(ItemGroups.TOOLS)
+                .modifyEntriesEvent(group)
                 .register((itemGroup) -> itemGroup.add(item));
     }
 
     public static void initialize() {
-        AddToTools(ModItems.DIAMOND_SPADE);
-        AddToTools(ModItems.DIAMOND_KNIFE);
+        AddToGroup(ModItems.DIAMOND_SPADE, ItemGroups.TOOLS);
+        AddToGroup(ModItems.DIAMOND_KNIFE, ItemGroups.COMBAT);
+        AddToGroup(ModItems.DIAMOND_PLATE, ItemGroups.INGREDIENTS);
+        AddToGroup(ModItems.REINFORCED_STICK, ItemGroups.INGREDIENTS);
     }
 }
